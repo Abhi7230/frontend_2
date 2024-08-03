dbf5c3d86355b0d6ed19e75bc5a44f4c2441fe9e
Abhi Raj <abhir0609@gmail.com>
2024-08-04 01:16:56 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider , ColorModeScript} from '@chakra-ui/react'
-
+//done
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ChakraProvider>
